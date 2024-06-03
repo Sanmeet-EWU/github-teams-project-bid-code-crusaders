712f08e49a19035a7e0acbc2396d2b32bfc562a9
Lane Keck <lkeck2@ewu.edu>
2024-06-03 10:49:04 -0700

    When you register, your profile is added to firestore database

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -20,3 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
+
+
